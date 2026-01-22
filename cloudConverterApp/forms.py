123b9ac4ab6fc7eeb3026a24da19bd3d7bb7e85b
anfal-darkbytes <anfaldarkbytes@gmail.com>
2026-01-22 18:30:07 +0500

    view and form section updated

--- cloudConverterApp/forms.py
+++ cloudConverterApp/forms.py
@@ -7,3 +7,7 @@ class OptionForm(forms.Form):
 class PickFileForm(forms.Form):
     file_picked = forms.FileField()
 
+class DataForm(forms.Form):
+    from_file = forms.CharField()
+    to_file = forms.CharField()
+    file_picked = forms.FileField()
--- cloudConverterApp/views.py
+++ cloudConverterApp/views.py
@@ -1,50 +1,36 @@
 from django.shortcuts import render
-from .forms import OptionForm, PickFileForm
+from .forms import OptionForm, PickFileForm, DataForm
 from .models import ConvertModel
 from .utils import detect_file_extension
+import logging
 
+logger = logging.getLogger('cloudConverterApp/views.py')
+converter = ConvertModel()
 
 def home(request):
-    if request.method == 'POST':
-        converter = ConvertModel()
-        # Options
-        if 'from_file' and 'to_file' in request.POST:
-            option_form = OptionForm(request.POST)
-            print(f'{request.POST}')
-            if option_form.is_valid():
-                from_file = option_form.cleaned_data['from_file']
-                to_file = option_form.cleaned_data['to_file']
-
-                converter.from_format = from_file
-                converter.to_format = to_file
-                converter.save(update_fields=['from_format', 'to_format'])
-
-
-        # Select File
-        if 'file_picked' in request.FILES:
-            select_form = PickFileForm(request.POST, request.FILES)
-            if select_form.is_valid():
-                selected_file = select_form.cleaned_data['file_picked']
-
-                converter.uploaded = selected_file
-
-                from_ext = detect_file_extension(selected_file)
-
-                converter.save(update_fields=['uploaded', 'from_format'])
-
-                to_ext = converter.objects.values_list('to_format')
-
-                context = {
-                    'from': from_ext,
-                    'to': to_ext,
-                    'file': selected_file
-                }
-
-                return render(request, 'converter/converter.html', context)
-
-
     return render(request, 'home/home.html')
 
+def data(request):
+    if request.method == 'POST':
+        data_form = DataForm(request.POST, request.FILES)
+        if data_form.is_valid():
+            to_file = data_form.cleaned_data['to_file']
+            file_picked = data_form.cleaned_data['file_picked']
+
+            from_file = detect_file_extension(file_picked)
+            converter.from_format = from_file
+            converter.to_format = to_file
+            converter.file_picked = file_picked
+            converter.save()
+
+            context = {
+                'from': from_file,
+                'to': to_file,
+                'file': file_picked
+            }
+
+            return context
+    return None
 
 def convert(request):
     return render(request, 'converter/converter.html')
